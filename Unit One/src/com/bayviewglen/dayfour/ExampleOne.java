eb9e6da17b63ce3169fc25093404546e34400ef1
gbaker <gbaker@gbakersp3.bayviewglen.ca>
2016-09-13 11:40:30 -0400

    Day Four Examples

---
+++ Unit One/src/com/bayviewglen/dayfour/ExampleOne.java
@@ -0,0 +1,21 @@
+package com.bayviewglen.dayfour;
+
+public class ExampleOne {
+
+	public static void main(String[] args) {
+		System.out.println(3 * 6); 
+		System.out.println(3 + 6); 
+		System.out.println(3 / 6); 
+
+		System.out.println(3.0 * 6); 
+		System.out.println(3 + 6.0); 
+		System.out.println(3.0 / 6); 
+										
+
+		System.out.println(3 % 6); 
+		System.out.println(23 % 2); 
+		System.out.println(458548 % 10); 
+
+	}
+
+}
